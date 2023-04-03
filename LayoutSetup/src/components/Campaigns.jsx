84d97ed1753ae4f9fa7570f9a1caf3ef364e8a8b
Stanyakimov <116120530+Stanyakimov@users.noreply.github.com>
2023-04-04 01:10:20 +0700

    Adds social interactions and shadow boxes

--- LayoutSetup/src/components/Campaigns.jsx
+++ LayoutSetup/src/components/Campaigns.jsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 
 import { campaigns } from '../resources/campaigns';
 import {
-    Card, CardHeader, CardTitle, CardBody, CardImage, CardFooter,
+    Card, CardHeader, CardTitle, CardBody, CardImage, CardFooter, CardSubtitle, CardActions,
 } from '@progress/kendo-react-layout';
+
 import { ChunkProgressBar } from '@progress/kendo-react-progressbars';
 
+import './styles.css';
 
 const Campaigns = () => {
 
@@ -20,12 +22,32 @@ const Campaigns = () => {
         background: 'royalBlue'
     };
 
+    function isWhatPercentOf(x, y) {
+        return ((x / y) * 100).toFixed(0);
+    }
+
+    // This function doesn't use the internationalization service provided by kendo
+    // We take the curent locale and we parse the date based on the globalization settings 
+    function formatDate(date) {
+        var date = new Date(date);
+        var today = new Date();
+
+        // request a weekday along with a long date
+        if (date.getFullYear() === today.getFullYear()) {
+            var options = { month: 'long', day: 'numeric' };
+            return date.toLocaleDateString("en-US", options);
+        } else {
+            var options = { year: 'numeric', month: 'long', day: 'numeric' };
+            return date.toLocaleDateString("en-US", options);
+        }
+    }
+
     return (
         <div id="Dashboard" className="dashboard-page main-content">
             <div className="card-container grid">
                 {data.map(cmp => (
                     // Recommended image size: 1920 x 1005 px
-                    <Card key={cmp.id} orientation="vertical" style={{
+                    <Card className="card-style" key={cmp.id} orientation="vertical" style={{
                         // width: "25em",
                     }}>
                         <CardImage src={cmp.mediaUrl} style={{
@@ -34,12 +56,28 @@ const Campaigns = () => {
                         }} />
                         <CardHeader>
                             <CardTitle>{cmp.title}</CardTitle>
-                            <ChunkProgressBar
-                                value={cmp.totalRaised}
-                                max={cmp.amountToRaise}
-                                emptyStyle={emptyStyles} progressStyle={progressStyles}
-                            />
-                            <CardFooter className="card-footer">{cmp.endDate}</CardFooter>
+                            <CardSubtitle>Campaign Completeness: {isWhatPercentOf(cmp.totalRaised, cmp.amountToRaise)}%</CardSubtitle>
+                            <span className='k-card-subtitle' style={{ float: 'right' }}>{formatDate(cmp.endDate)}</span>
+
+                            <CardBody style={{ paddingLeft: '0px' }}>
+                                <ChunkProgressBar
+                                    style={{ marginTop: '0.5em' }}
+                                    value={cmp.totalRaised}
+                                    max={cmp.amountToRaise}
+                                    chunkCount={10}
+                                    emptyStyle={emptyStyles} progressStyle={progressStyles}
+                                />
+                            </CardBody>
+                            <CardFooter style={{ paddingLeft: '0px' }}>
+                                <button className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-primary">
+                                    Review
+                                </button>
+                                <ul className='social-interactions'>
+                                    <li className='k-icon k-i-eye' style={{ marginLeft: '4em' }}>1450</li>
+                                    <li className='k-icon k-i-fav-outline'>365</li>
+                                    <li className='k-icon k-i-comment'>16</li>
+                                </ul>
+                            </CardFooter>
                         </CardHeader>
                     </Card>
                 ))}
